c96fbbc0d0b957099f6114841751e9cd287216c1
Anwar Shaikh <anwarshaikh@microsoft.com>
2023-10-24 00:11:57 +0530

    Initial commit

---
+++ Anwartest_.py
@@ -0,0 +1,29 @@
+#varibales in python
+#rules of creating varibles in python
+'''
+1. variable should start with letter or underscore
+2. variable can not start with number
+3. it can only contain alpha, and numberic character
+4. variable names are case sensative
+'''
+'''import time
+
+print("Hello There!")
+print("here you go")
+print("Here is the case sensative example")
+a='happy'
+b='Happy'
+print(a+b)
+
+#assigning value into varibales
+a=100
+b='Anwar'
+c=22.33
+print(a)
+print(b)
+print(c)
+time.sleep(5) '''
+
+ab=int(input("Enter first Number: "))
+cd=int(input("Enter Second Number: "))
+print("The total of two number is",ab+cd)
